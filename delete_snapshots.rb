96a91e162049b1d71a4b1dd20a26e1ff309ca79a
akiraisomura <a.isomura5160@gmail.com>
2022-06-17 15:24:18 +0900

    feat: can get nonexistent ami

--- delete_snapshots.rb
+++ delete_snapshots.rb
@@ -4,7 +4,7 @@ require 'aws-sdk'
 require_relative './list_snapshots_without_volume.rb'
 
 def main
-  snapshots = list_snapshots_without_volume
+  snapshots = list_unused_snapshots
   snapshots.each { |s| client.delete_snapshot(snapshot_id: s.snapshot_id) }
 end
 
